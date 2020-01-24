9d5da6d5406ac52262571b599977b30f0fee3b68
Michael De Madet <michaeldemadet@gmail.com>
2020-01-24 17:57:25 +0100

    Add User entity and dto

--- backend/src/main/java/fr/formation/backend/dtos/UserDto.java
+++ backend/src/main/java/fr/formation/backend/dtos/UserDto.java
@@ -1,4 +1,33 @@
 package fr.formation.backend.dtos;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserDto {
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    @Size(min = 8)
+    private String password;
+
+    public UserDto() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
