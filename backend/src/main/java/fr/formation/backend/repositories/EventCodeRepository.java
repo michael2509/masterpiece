e5b69b56e4bcfaba780c0d4f357ddb5ddd8c30bc
Michael De Madet <michaeldemadet@gmail.com>
2020-07-20 11:31:18 +0200

    add hashids dependency

--- backend/src/main/java/fr/formation/backend/repositories/EventCodeRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.formation.backend.repositories;
-
-import fr.formation.backend.entities.EventCode;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventCodeRepository extends JpaRepository<EventCode, Long> {
-}
---
+++ backend/src/main/java/fr/formation/backend/viewdtos/EventViewDto.java
@@ -0,0 +1,4 @@
+package fr.formation.backend.viewdtos;
+
+public class EventViewDto {
+}
