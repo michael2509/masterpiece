952be46906aeec1488f6cd48d3065641703e416c
Michael De Madet <michaeldemadet@gmail.com>
2020-08-05 23:08:12 +0200

    remove unused imports

--- backend/src/main/java/fr/formation/backend/config/AppConfig.java
+++ backend/src/main/java/fr/formation/backend/config/AppConfig.java
@@ -1,9 +1,6 @@
 package fr.formation.backend.config;
 
-import fr.formation.backend.dtos.EventDto;
-import fr.formation.backend.entities.Event;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
