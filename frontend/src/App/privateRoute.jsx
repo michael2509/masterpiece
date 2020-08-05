b04626d4e4bae443149ad36e809c93a44e8047e2
Michael De Madet <michaeldemadet@gmail.com>
2020-08-05 19:53:54 +0200

    use isLogged function

--- frontend/src/App/privateRoute.jsx
+++ frontend/src/App/privateRoute.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { isLogged } from '../Auth/authService';
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
+export const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route {...rest} render={props => (
-        localStorage.getItem("access_token") ? <Component {...props} /> : <Redirect to={{ pathname: '/connexion', state: { from: props.location } }} />
+        isLogged() ? <Component {...props} /> : <Redirect to={{ pathname: '/connexion', state: { from: props.location } }} />
     )} />
 )
 
